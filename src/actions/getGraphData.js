8fc9bf52af98997e3a936b8e463256c05ba6b833
Павел <elantsev.pa@gmail.com>
2019-11-06 10:05:16 +0300

    getGraphData

--- src/actions/getGraphData.js
+++ src/actions/getGraphData.js
@@ -2,23 +2,23 @@ export const GET_GRAPH_DATA_IS_LOADING = "GET_GRAPH_DATA_IS_LOADING";
 export const GET_GRAPH_DATA_SUCCESS = "GET_GRAPH_DATA_SUCCESS";
 export const GET_GRAPH_DATA_ERROR = "GET_GRAPH_DATA_ERROR";
 
-export function getGraphData() {
-  return { type: GET_GRAPH_DATA_SUCCESS };
-}
+const data = {
+  day: [40, 19, 8],
+  month: [402, 180, 180],
+  year: [5000, 615, 1500]
+};
 
-// export function fetchCustomNews(source, relevance) {
-//   return async function(dispatch) {
-//     try {
-//       dispatch({ type: "FETCH_CUSTOM_NEWS_IS_LOADING" });
-//       let response = await fetch(
-//         `https://newsapi.org/v1/articles?source=${source}&sortBy${relevance}+&apiKey=${API_KEY}`
-//       );
-//       if (response.status === 200) {
-//         response = await response.json();
-//         dispatch({ type: "FETCH_CUSTOM_NEWS", payload: response.articles });
-//       }
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-// }
+export function getGraphData() {
+  return function(dispatch) {
+    dispatch({ type: GET_GRAPH_DATA_IS_LOADING });
+    try {
+      setTimeout(
+        () => dispatch({ type: GET_GRAPH_DATA_SUCCESS, payload: data }),
+        4000
+      );
+    } catch (error) {
+      console.log(error);
+      dispatch({ type: GET_GRAPH_DATA_ERROR, payload: error });
+    }
+  };
+}
